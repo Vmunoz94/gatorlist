1803a2115273a7bded218bb76da33ecb4d466e91
karuna-nayak <nayak.karu@gmail.com>
2019-02-18 18:40:01 -0800

    upadated page Karuna text body

--- Team04App/resources/views/names/karuna.blade.php
+++ Team04App/resources/views/names/karuna.blade.php
@@ -8,7 +8,14 @@
 @section('imgSource', '/images/karuna.jpg')
 @section('cardBody')
     <div class="lead">
-        Hi.. This is Karuna Nayak. Welcome to my page. I am a computer science graduate student at SFSU. 
+        Hi.. This is Karuna Nayak. Welcome to my page. I am a computer science graduate student at SFSU. This is my 2nd semester as a grad student. 
     </div>
-    
+    <div class="lead">
+        This semester I am currently enrolled in:
+    </div>
+    <ul class="ml-5 border-left">
+        <li>Software Engineering</li>
+        <li>Data Mining</li>
+        <li>Independent Study</li>
+    </ul>
 @endsection

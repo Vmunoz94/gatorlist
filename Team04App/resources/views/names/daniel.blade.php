52e2eb1969b20c106e525b14a8389f979d99a8f3
Daniel Mossaband <DanMossa@users.noreply.github.com>
2019-02-18 22:30:26 -0800

    Revert "Finished About Controller and my profile"

--- Team04App/resources/views/names/daniel.blade.php
+++ Team04App/resources/views/names/daniel.blade.php
@@ -5,18 +5,12 @@
 @section('title', 'Victor')
 @section('jumbotronTitle', 'Daniel Mossaband')
 @section('job', 'Backend Lead')
-@section('imgSource', '/images/danielProfilePicture.jpg')
+<!--@section('imgSource', '/images/IMAGEFILE.jpg')-->
 @section('cardBody')
 <div class="lead">
-    My name is Daniel Mossaband and I've just recently become a resident of San Francisco. I moved to San Francisco
+    Hey! My name is Daniel Mossaband and I've just recently become a resident of San Francisco. I moved to San Francisco
     from Los Angeles in Spring of 2017 to pursue my passion in Computer Science and hope to graduate by Spring of 2019
     from San Francisco State University.
 </div>
-
-<ul class="ml-5 border-left">
-    <li><a target="_blank" href="https://Mossaband.com/">Mossaband.com</a></li>
-    <li><a target="_blank" href="https://Github.com/DanMossa">GitHub.com</a></li>
-    <li><a target="_blank" href="https://linkedin.com/in/danmossa">LinkedIn.com</a></li>
-</ul>
 
 @endsection

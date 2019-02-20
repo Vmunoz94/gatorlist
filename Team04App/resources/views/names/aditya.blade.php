cdbb6b93fb5e0b9429f6dc8da13b86414a8afe2f
Aditya Sheoran <adityasheoran@adityas-air.attlocal.net>
2019-02-19 22:24:34 -0800

    <Aditya's about page>

---
+++ Team04App/resources/views/names/aditya.blade.php
@@ -0,0 +1,30 @@
+{{-- use the aboutNamesLayout --}}
+@extends('layouts.names')
+
+{{-- fill in values that were left empty in the layout --}}
+@section('title', 'Aditya')
+@section('jumbotronTitle', 'Aditya Sheoran')
+@section('job', 'Backend')
+@section('imgSource', '/images/adityaPic.jpg')
+@section('cardBody')
+    <div class="lead">
+        I'm Aditya, and this is my final semester at San Francisco State University. Some of the elective courses I have
+        taken here at SFSU are:
+    </div>
+    <ul class="ml-5 border-left">
+        <li>Computer Organization</li>
+        <li>Programming cafe</li>
+        
+    </ul>
+    <div class="lead">
+        This semester I am currently enrolled in five CSC clases, which are:
+    </div>
+    <ul class="ml-5 border-left">
+        <li>Software Engineering</li>
+        <li>Introduction to Database Systems</li>
+        <li>Analysis of Algorithms</li>
+        <li>Programming Languages</li>
+        <li>System Administration</li>
+
+    </ul>
+@endsection

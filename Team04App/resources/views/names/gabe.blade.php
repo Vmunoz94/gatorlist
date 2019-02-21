42bcf237a9493eb8432cea7855719568f1b917d3
GabrielAlfaro <alfaro1123@gmail.com>
2019-02-20 20:35:28 -0800

    gabes home page

---
+++ Team04App/resources/views/names/gabe.blade.php
@@ -0,0 +1,16 @@
+{{-- use the aboutNamesLayout --}}
+@extends('layouts.names')
+
+{{-- fill in values that were left empty in the layout --}}
+@section('title', 'Gabe')
+@section('jumbotronTitle', 'Gabriel Alfaro')
+@section('job', 'Backend')
+<!--@section('imgSource', '/images/IMAGEFILE.jpg')-->
+@section('cardBody')
+<div class="lead">
+    Hello, my name is Gabriel Alfaro and I am a 5th year currently attending
+    SFSU and I plan to graduate this semester Spring 2019. I hope to learn as
+    much as possible when it comes to Computer Science.
+</div>
+
+@endsection

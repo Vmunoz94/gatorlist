641de438b997369e8f0f2ac5de63df3016ae0753
Victor Munoz <Vmunoz94@hotmail.com>
2019-04-11 19:41:00 -0700

    forgot to recompile

--- Team04App/public/js/app.js
+++ Team04App/public/js/app.js
@@ -2912,29 +2912,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({

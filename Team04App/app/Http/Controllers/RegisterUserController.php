014c0ce88db3dc45cfae8c0690db3261799c32b6
Aditya <asheoran7885@gmail.com>
2019-05-15 15:42:13 -0700

    added validators to registor

--- Team04App/app/Http/Controllers/RegisterUserController.php
+++ Team04App/app/Http/Controllers/RegisterUserController.php
@@ -16,16 +16,31 @@ class RegisterUserController extends Controller
     public function store(Request $request)
     {
         $firstName = $request->input('firstName');
+        // if(empty($firstName['firstName'])){
+        //     return "first name field is empty";
+        // }
         $lastName = $request->input('lastName');
         $userName = $request->input('userName');
 
         $emailAddr = $request->input('emailAddr');
-        if(!strpos($emailAddr, '@')){
-            return "Email requires @ sign";
-        }
+
+        // if(!strpos($emailAddr, '@')){
+        //     return "Email requires @ sign";   
+        // }
+        
 
         $password = $request->input('password');
         $securePassword = bcrypt($password);
+
+        if(empty($firstName) || empty($lastName) || empty($emailAddr) || empty($userName) || empty($password)){
+
+            return "One or more field is empty!. Please fill all the Fields!.";
+        } else if (!strpos($emailAddr, '@')){
+            return "Email requires @ sign";
+        } 
+        // else if($userName>0){
+        //     return "Username already exist, use a different username.";
+        // }
 
         $result = DB::table('users')->insert(
             [
@@ -36,6 +51,7 @@ class RegisterUserController extends Controller
                 'password' => $securePassword,
                 'accountCreation' => date("Y-m-d H:i:s")
             ]
+
         );
         return "Finished";
 

5c04184d912b05eb8e89de8c44b35a5c345cecc1
GabrielAlfaro <alfaro1123@gmail.com>
2019-05-17 23:56:42 -0700

    all the times I tried to access database

--- Team04App/app/Http/Controllers/LoginUserController.php
+++ Team04App/app/Http/Controllers/LoginUserController.php
@@ -7,24 +7,70 @@ use Illuminate\Support\Facades\DB;
 
 class LoginUserController extends Controller
 {
-
-
     // public function index(Request $request)
     // {
-
     // }
-
 public function store(Request $request){
     $userName = $request->input('userName');
     $password = $request->input('password');
+    //
 
+
+    //$queryResult = DB::table('users')->select('userName')->get();
+    //$queryResult = DB::table('users')->select('select * from users where userName','=',$userName);
+   // return $queryResult;
+//    $passwordResult = DB::table('users')->where('userName', $userName)->get('password');
+//    $passwordResult = $passwordResult->pluck('password')->toArray();
+  // return $passwordResult;
+//   $passwordResult = DB::table('users')->where('userName', $userName)->select('password')->get();
+//   $passwordResult = DB::table('users')->where('userName', $userName)->pluck('password')->toArray();
+// $passwordResult = DB::table('users')->where('userName', $userName)->select('password')->get();
+// $passwordResult = DB::table('users')->where('userName', $userName)->pluck('password')->toArray();
+// return $passwordResult;
+
+// $passwordResult = DB::table('users')->where('userName', $userName)->get(['password'])->toArray();
+// return $passwordResult;
+// $passwordResult = DB::table('users')->where('userName', $userName)->select('password')->get();
+// $passwordResult = DB::table('users')->where('userName', $userName)->pluck('password')->toArray();
+//echo $passwordResult;
+//$passwordResult = "";
+// $passwordResult = DB::table('users')->where('userName', $userName)->get('password');
+//  $passwordResult1 = $passwordResult->pluck('password')->toArray();
+// $passwordResult = DB::table('users')->select('select * from users where','userName','=',$userName);
+// $passwordResult1 = $passwordResult['password'];
+// foreach($passwordResult as $x=>$x_value){
+//     echo $x;
+// }
+// return $passwordResult1;
+//echo $passwordResult1;
+//$passwordResult = DB::table('')
+// $passwordResult = DB::table('users')->select('userName')->where('userName','=',$userName);
+// return $passwordResult;
+// $passwordResult = DB::table('team04db')->select('userName')->where('userName','=',$userName)->get('password');
+// return $passwordResult;
+// echo $userName;
+// echo $password;
     //Receive hashed password from database
-$queryResult = DB::table('users')->where('userName', $userName)->get();
-$passwordResult = $queryResult["password"];
-if(password_verify($password, $passwordResult)){
+//$userResult = DB::table('users')->where('userName', $userName)->get('userName')->toArray();
+//$passwordResult = DB::table('users')->where('userName', $userName)->get('password')->toArray();
+//$passwordResult = DB::table('users')->where('userName', $userName)->value('password');
+
+//$passwordResult = DB::table('users')->pluck('password');
+//echo $passwordResult;
+//$passwordResult = $queryResult["password"];
+$passwordResult = DB::table('users')->where('userName','=',$userName)->get()->toArray();
+
+
+
+if(password_verify($password, $passwordResult)){//password_verify: bool
     return "Success";
+    //echo "Success for Password";
 }else{
-    return "Fucked up password";
+    return "Incorrect Password";
+    //echo "Not working";
 }
+
 }
+
+
 }
